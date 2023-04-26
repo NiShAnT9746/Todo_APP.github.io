e322065b4fb08faa815d1899eb801bc19e3094d8
NiShAnT9746 <121337077+NiShAnT9746@users.noreply.github.com>
2023-04-26 10:19:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ listContainer.addEventListener("click",function(e){
         e.target.parentElement.remove();
         SaveData();
     }
-    },false);
+    },);
     function SaveData(){
         localStorage.setItem("data", listContainer.innerHTML);
     }
